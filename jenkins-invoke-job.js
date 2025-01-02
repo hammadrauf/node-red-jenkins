524cf1c57fac1fa03be986551c4ef6db7c64ec9f
Hammad Rauf <rauf.hammad@gmail.com>
2025-01-02 16:07:23 -0500

    wip

--- jenkins-invoke-job.js
+++ jenkins-invoke-job.js
@@ -14,60 +14,38 @@ module.exports = function(RED) {
             try {
                 node.status({fill:'green', shape:'ring', text:'initializing....'});
 
-                // if (msg.hasOwnProperty('jobname')) {
-                //     config.jobname = msg.jobname;
-                // }
+                if (job.hasOwnProperty('paramsarray')) {
+                     this.paramsarray = job.paramsarray;
+                }
 
-                // const urlParts = node.connection.baseUrl.split('://');
-                // const jenkinsClient = new Jenkins({
-                // // form the url with the username and password
-                // baseUrl: urlParts[0] + '://' + node.connection.username + ':' + node.connection.password + '@' + urlParts[1],
-                // });
+                const urlParts = node.connection.baseUrl.split('://');
+                const jenkinsClient = new Jenkins({
+                // form the url with the username and password
+                baseUrl: urlParts[0] + '://' + node.connection.username + ':' + node.connection.password + '@' + urlParts[1],
+                });
         
                 node.status({fill:'green', shape:'ring', text:'calling....'});
 
-                // // method is config.method and can be 'jobs.get'
-                // let method = jenkinsClient;
-                // let caller;
-                // apiMethodBuild.split('.').forEach(m => {
-                //     caller = method;
-                //     method = method[m];
-                // });
+                // method is config.method and can be 'jobs.get'
+                let method = jenkinsClient;
+                let caller;
+                apiMethodBuild.split('.').forEach(m => {
+                    caller = method;
+                    method = method[m];
+                });
         
-                // // params can be an array if multiple params are needed
-                // let params = config.jobname;
-                // if (!Array.isArray(params)) {
-                //     // maybe String only  
-                //     params = [params];
-                // }          
+                // params can be an array if multiple params are needed
+                let params = this.paramsarray;
+                if (!Array.isArray(params)) {
+                    params = [params];
+                }          
 
-                // // call method with params, bind to caller
-                // xml = await method.apply(caller, params);
-                // //const xml = data.xml;
-
-                // const dom = new JSDOM(xml, { contentType: "text/xml" });
-                // const xmlDoc = dom.window.document;
-                // const results = [];
                 var msg = new Object();
-                // reEx = /hudson\.model\..*ParameterDefinition/
+
+                // call method with params, bind to caller
+                invoke_result = await method.apply(caller, params);
                 
-                // const allElements = xmlDoc.getElementsByTagName("*");
-                // const matchedElements = [];
-              
-                // for (let element of allElements) {
-                //   const myObj = new Object();
-                //   if (reEx.test(element.tagName)) {
-                //     const fulltagname = element.tagName;
-                //     // extract standard DataType from Hudson Datatype String, empty if fails.
-                //     myObj.dataType = fulltagname.match(/hudson\.model\.(.*)ParameterDefinition/)[1] || [""];
-                //     //console.log(element['name']);
-                //     myObj.name = element.getElementsByTagName("name")[0].innerHTML;
-                //     myObj.defaultValue = element.getElementsByTagName("defaultValue")[0] ? element.getElementsByTagName("defaultValue")[0].innerHTML : ""
-                //     matchedElements.push(myObj);   
-                //   }
-                // }
-                // const elements = matchedElements;               
-                // msg.payload = elements;
+                msg.payload = invoke_result;
 
                 node.status({fill:'green', shape:'dot', text:'Done'});
                 node.send(msg);
